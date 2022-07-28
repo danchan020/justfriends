009ad55eddea6e402c92c031b278b69d13d80a6c
Daniel <danchan020@gmail.com>
2022-07-28 11:00:17 -0700

    before i break

--- client/src/components/OtherProfile.js
+++ client/src/components/OtherProfile.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import { VStack, Avatar, Text, Tag } from '@chakra-ui/react'
-import { useSelector } from 'react-redux'
-import { selectUser } from  '../features/user'
 import TopBar from './TopBar'
 
 export default function OtherProfile({profile, handleSignOut}) {
-  console.log(profile)
 
   return (
     <>
       <TopBar handleSignOut={handleSignOut}/>
       <VStack>
-        <Text>  Profile </Text>
+        <Text>  {profile.first_name}'s Profile </Text>
         <Avatar size='2xl' src={profile.avatar} />
         <Text> { profile.first_name}{' '}{profile.last_name} </Text>
         <Tag> @{ profile.username}</Tag>
--- client/src/components/UserFeatures.js
+++ client/src/components/UserFeatures.js
@@ -9,7 +9,7 @@ export default function UserFeatures({handleSignOut, users, handleProfileClick})
   const user = useSelector(selectUser)
 
   let renderOtherUsers
-  if (users) {renderOtherUsers = users.map((user) => {
+  if (users){renderOtherUsers = users.map((user) => {
     return (
       <UserCard key={user.id}{...user} handleProfileClick={handleProfileClick}/>
       )
